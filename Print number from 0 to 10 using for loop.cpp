ddd19abaccacac47078fb817099c07086e05c922
Prathyush2007 <prathyush.nimmagadda.btech2024@sitpune.edu.in>
2025-08-05 01:05:15 +0530

    Create Print number from 0 to 10 using for loop.cpp

---
+++ Print number from 0 to 10 using for loop.cpp
@@ -0,0 +1,12 @@
+//Name : Prathyush Nimmagadda
+//PRN : 24070123064
+//Batch : ENTC A3
+#include <iostream>
+using namespace std;
+
+int main(){
+    for (int i=0;i<=10;i++) 
+    {if(i%2==0){cout<<i<<endl;}
+    }
+    return 0;
+}
