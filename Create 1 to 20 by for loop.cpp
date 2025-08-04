df6bbbc98454ad2980cae5fbb6795d14284976e3
Prathyush2007 <prathyush.nimmagadda.btech2024@sitpune.edu.in>
2025-08-05 01:10:28 +0530

    Create Create 1 to 20 by for loop.cpp

---
+++ Create 1 to 20 by for loop.cpp
@@ -0,0 +1,10 @@
+//Name : Prathyush Nimmagadda
+//PRN : 24070123064
+//Batch : ENTC A3
+#include <iostream>
+using namespace std;
+int main(){
+    int i=0;
+    while(i<20){cout<<i+1<<" ";i++;cout<<endl;}
+    return 0;
+}
