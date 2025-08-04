147f86403c013405ae34714ce2ee85bb7555ddf2
Prathyush2007 <prathyush.nimmagadda.btech2024@sitpune.edu.in>
2025-08-05 01:08:38 +0530

    Create Hourglass star pattern.cpp

---
+++ Hourglass star pattern.cpp
@@ -0,0 +1,29 @@
+//Name : Prathyush Nimmagadda
+//PRN : 24070123064
+//Batch : ENTC A3
+#include <iostream>
+using namespace std;
+int main() {
+    int n;
+    cout << "Enter the number of rows for the upper half: ";
+    cin >> n;
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < i; j++) {
+            cout << " ";
+        }
+        for (int k = 0; k < 2 * (n - i) - 1; k++) {
+            cout << "*";
+        }
+        cout << endl;
+    }
+    for (int i = 1; i < n; i++) {
+        for (int j = 0; j < n - i - 1; j++) {
+            cout << " ";
+        }
+        for (int k = 0; k < 2 * i + 1; k++) {
+            cout << "*";
+        }
+        cout << endl;
+    }
+    return 0;
+}
