10534285c581f5105251726b5ac41878cf097fc1
Prathyush2007 <prathyush.nimmagadda.btech2024@sitpune.edu.in>
2025-08-05 01:04:03 +0530

    Create To print SIT 5 times using for loop.cpp

---
+++ To print SIT 5 times using for loop.cpp
@@ -0,0 +1,12 @@
+// Name : Prathyush Nimmagadda
+//PRN : 24070123064
+//Batch : ENTC A3
+#include <iostream>
+using namespace std;
+
+int main(){
+    for (int i=0;i<=5;i++) 
+    {cout<<"SIT"<<endl;}
+    
+    return 0;
+}
