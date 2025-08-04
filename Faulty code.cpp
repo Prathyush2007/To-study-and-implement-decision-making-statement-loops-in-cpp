a0214baad0aad24d579ef4d00c97c300c5ebd36c
Prathyush2007 <prathyush.nimmagadda.btech2024@sitpune.edu.in>
2025-08-05 01:09:50 +0530

    Create Faulty code.cpp

---
+++ Faulty code.cpp
@@ -0,0 +1,15 @@
+// Name : Prathyush Nimmagadda
+//PRN : 24070123064
+//Batch: ENTC A3
+#include <iostream>
+using namespace std;
+int main() {
+    int i, j;
+    int n = 5;
+    for (int i = 1; i <= n; i++) {
+        for (int j = 1; j <= i; j++) {
+            cout << "* ";
+            cout << endl;
+    }
+    return 0;
+}
